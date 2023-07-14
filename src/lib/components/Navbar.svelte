1246645d1b71356875e26edc18fa657846d8e801
Samuel Calderon <samuel.calderon@uarm.pe>
2023-07-14 15:07:20 -0500

    add navbar content

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand fs-3" href="{base}/">
+        <img src="logo-almacen.svg" alt="logo" width="30px" height="40px">
+        Almacén DDC
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="{base}/about"><i class="bi bi-info-circle pe-1 fs-5"></i>Info</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://github.com/calderonsamuel/ddc-almacen"><i class="bi bi-github pe-1 fs-5"></i>Repo</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Navbar from "$lib/components/Navbar.svelte";
+</script>
+
+<svelte:head>
+    <title>Gestión de almacen - DDC</title>
+    <meta name="description" content="Aplicativo para llevar el control de los ingresos al almacén de la DDC-MININTER">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <!-- bootstrap-icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</svelte:head>
+
+<Navbar />
+
+<slot></slot>
